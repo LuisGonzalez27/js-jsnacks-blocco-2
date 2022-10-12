4408e68c590986257ed822e59faf57ba8adecf12
LuisGonzalez27 <luis27coding@gmail.com>
2022-10-12 18:53:47 +0200

    SNACK 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 "use strict";
+// SNACK 1
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 const bottone = document.getElementById('btn');
@@ -16,3 +17,22 @@ function(){
     document.getElementById('numeroInserito').value = '';
 }
 );
+
+// // SNACK 2
+// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di 
+// cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+const arrayNomi = ['Luis', 'Miguel', 'Kin', 'Silvia', 'Antonio', 'Giulia'];
+const arrayCognomi = ['Gonzalez', 'Castellanos', 'Panaia', 'Furia', 'Garzia'];
+const listaFalsa = document.getElementById('lista');
+
+const generate = function (){
+    document.querySelector('ul').innerHTML = '';
+    for(let i = 0; i < 5; i++){
+    let invitati = document.createElement('li');
+    let nomeInvitati = arrayNomi[Math.floor(Math.random() * (arrayNomi.length))] + ' ' + arrayCognomi[Math.floor(Math.random() * (arrayCognomi.length))];
+    invitati.append(nomeInvitati);
+    document.querySelector('ul').append(invitati)
+    }
+}
+listaFalsa.addEventListener('click', generate);
